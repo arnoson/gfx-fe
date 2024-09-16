4ec4cd9491308a6fe73067e835e954ab819f72fa
arnoson <hallo@arno-schlipf.info>
2024-09-16 17:03:28 +0200

    chore: use subfolder for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue(), svgLoader()],
+  base: mode === 'development' ? '/' : '/gfx-fe/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
